9b0a3396cd1b89848b69d9a61daed60f3bc36413
Felipe Adriano <adrianofelipe2004@gmail.com>
2025-07-01 14:41:36 -0300

    Projeto Mobile inicla

---
+++ pages/resume.js
@@ -0,0 +1,19 @@
+import { ScrollView, View, StatusBar, StyleSheet } from "react-native";
+
+export default function Resume() {
+  return (
+    <View>
+      <Text>Open up App.js to start working on your app!</Text>
+      <StatusBar />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
